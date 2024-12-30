b3225f958257b392a860b1fed892cdb3de11a3dd
Mahajabin Tokon <tokonislam1122@gmail.com>
2024-12-29 17:57:24 -0700

    Update

--- src/Components/Featured/MenuCategory.jsx
+++ src/Components/Featured/MenuCategory.jsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React from "react";
+import MenuItem from "../../Shared/MenuItem";
+import Cover from "../../Shared/Cover";
 
-const MenuCategory = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+const MenuCategory = ({items, title, img}) => {
+  return (
+    <div className="py-10">
+      {title && <Cover img={img} title={title}></Cover>}
+      <div className="grid md:grid-cols-2 gap-10 my-10">
+        {items.map((item) => (
+          <MenuItem key={item._id} item={item}></MenuItem>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default MenuCategory;
--- src/Pages/Menu.jsx
+++ src/Pages/Menu.jsx
@@ -1,21 +1,50 @@
-import React from 'react';
-import { Helmet } from 'react-helmet-async';
-import Cover from '../Shared/Cover';
-import coverImg from "../assets/menu/banner3.jpg"
-import PopularMenu from '../Components/PopularMenu';
+import React from "react";
+import { Helmet } from "react-helmet-async";
+import Cover from "../Shared/Cover";
+import coverImg from "../assets/menu/banner3.jpg";
+import mImgDessert from "../assets/menu/dessert-bg.jpeg";
+import mImgSoup from "../assets/menu/soup-bg.jpg";
+import mImgSalad from "../assets/menu/salad-bg.jpg";
+import mImgPizze from "../assets/menu/pizza-bg.jpg";
+import useMenu from "../Hooks/useMenu";
+import MenuCategory from "../Components/Featured/MenuCategory";
+import SectionTitle from "../Components/SectionTitle";
 
 const Menu = () => {
-    return (
-        <div>
-            <Helmet><title>Bistro Boss | Menu</title></Helmet>
-            <Cover img={coverImg} title="our menu"></Cover>
-            <PopularMenu></PopularMenu>
-            <Cover img={coverImg} title="our menu"></Cover>
-            <PopularMenu></PopularMenu>
-            <Cover img={coverImg} title="our menu"></Cover>
-            <PopularMenu></PopularMenu>
-        </div>
-    );
+  const [menu] = useMenu();
+  const dessert = menu.filter((item) => item.category === "dessert");
+  const soup = menu.filter((item) => item.category === "soup");
+  const salad = menu.filter((item) => item.category === "salad");
+  const pizza = menu.filter((item) => item.category === "pizza");
+  const offered = menu.filter((item) => item.category === "offered");
+  return (
+    <div>
+      <Helmet>
+        <title>Bistro Boss | Menu</title>
+      </Helmet>
+      <SectionTitle
+        subHeading="Our Menu"
+        heading="Today's Offer"
+      ></SectionTitle>
+      <MenuCategory items={offered}></MenuCategory>
+      <MenuCategory
+        items={dessert}
+        title={"Dessert"}
+        img={mImgDessert}
+      ></MenuCategory>
+      <MenuCategory items={soup} title={"Soup"} img={mImgSoup}></MenuCategory>
+      <MenuCategory
+        items={salad}
+        title={"salad"}
+        img={mImgSalad}
+      ></MenuCategory>
+      <MenuCategory
+        items={pizza}
+        title={"pizza"}
+        img={mImgPizze}
+      ></MenuCategory>
+    </div>
+  );
 };
 
 export default Menu;
