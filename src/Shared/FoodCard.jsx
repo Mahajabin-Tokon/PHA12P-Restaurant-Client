a983b3b75a50c87c360996fd77be5be8ab4f6279
Mahajabin Tokon <tokonislam1122@gmail.com>
2025-01-01 12:57:13 -0700

    Update

--- src/Shared/FoodCard.jsx
+++ src/Shared/FoodCard.jsx
@@ -15,7 +15,7 @@ const FoodCard = ({ item }) => {
         <h2 className="card-title">{name}</h2>
         <p>{recipe}?</p>
         <div className="card-actions justify-end">
-          <button className="btn btn-primary">Order Now</button>
+          <button className="btn btn-outline bg-slate-100 border-orange-400 border-0 border-b-4 mt-4">Order Now</button>
         </div>
       </div>
     </div>
