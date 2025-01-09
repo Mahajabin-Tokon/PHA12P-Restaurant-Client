b914dec318925889b42b6f55f0802306d50801a5
Mahajabin Tokon <tokonislam1122@gmail.com>
2025-01-09 14:42:18 -0700

    update

--- src/Pages/Dashboard/PaymentHistory.jsx
+++ src/Pages/Dashboard/PaymentHistory.jsx
@@ -14,7 +14,35 @@ const PaymentHistory = () => {
     },
   });
 
-  return <div><h2>Total Payments: {payments.length}</h2></div>;
+  return (
+    <div>
+      <h2>Total Payments: {payments.length}</h2>
+      <div className="overflow-x-auto">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Email</th>
+              <th>Transaction ID</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* row 1 */}
+            {payments.map((eachPayment, index) => (
+              <tr key={eachPayment._id}>
+                <th>{index+1}</th>
+                <td>{eachPayment.email}</td>
+                <td>{eachPayment.transactionId}</td>
+                <td>{eachPayment.status}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default PaymentHistory;
